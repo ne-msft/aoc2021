164ceddf368598f6efd809a13523f2580a1482e0
Nico Erfurth <nierfurt@microsoft.com>
2021-12-01 22:51:58 +0100

    Solver for Day01

---
+++ solver_day01_1/Program.cs
@@ -0,0 +1,37 @@
+﻿
+// Read file and convert to list of integers
+var listOfMeasurements = System.IO.File.ReadLines(@"input1.txt")
+    .Select(x => int.TryParse(x, out int i) ? i : (int?)null)
+    .Where(x => x.HasValue)
+    .Select(x => x.Value)
+    .ToList();
+
+/// <summary>
+/// Process list and return number of items which had smaller precessors.
+/// </summary>
+int solveDay01(IList<int> input)
+{
+    var numberOfIncreasingValues =
+        input.Aggregate((last: int.MaxValue, count: 0),
+            (runningMemory, nextValue) =>
+            {
+                if (runningMemory.last < nextValue) { return (nextValue, runningMemory.count + 1); };
+                return (nextValue, runningMemory.count);
+            },
+            result => result.count);
+
+    return numberOfIncreasingValues;
+}
+
+/// <summary>
+/// Create a list which creates a sliding window sum over 3 values.
+/// </summary>
+IList<int> creatingSlidingWindowList(IList<int> input)
+{
+    return input.SkipLast(2).Select((val, index) => 
+        val + input[index + 1] + input[index + 2]
+    ).ToList();
+}
+
+Console.WriteLine($"Test1: Total: {listOfMeasurements.Count} Increasing: {solveDay01(listOfMeasurements)}");
+Console.WriteLine($"Test2: Total: {listOfMeasurements.Count} Increasing: {solveDay01(creatingSlidingWindowList(listOfMeasurements))}");
